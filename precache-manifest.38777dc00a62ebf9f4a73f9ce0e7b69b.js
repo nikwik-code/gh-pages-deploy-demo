27dd94580803151114b291624b1fb12b15cc05f0
Niklas Wikström <niklas.wikstrom@learnifier.com>
2020-07-02 16:16:38 +0200

    Updates

---
+++ precache-manifest.38777dc00a62ebf9f4a73f9ce0e7b69b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27fabd4317f63d82cdcc20c5032a4c4",
+    "url": "/gh-pages-deploy-demo/index.html"
+  },
+  {
+    "revision": "4aa78d0404e7b30b3a62",
+    "url": "/gh-pages-deploy-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74f1977fee84267dd621",
+    "url": "/gh-pages-deploy-demo/static/js/2.3428cfce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-deploy-demo/static/js/2.3428cfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa78d0404e7b30b3a62",
+    "url": "/gh-pages-deploy-demo/static/js/main.17b5c38e.chunk.js"
+  },
+  {
+    "revision": "5e52bcef712809035956",
+    "url": "/gh-pages-deploy-demo/static/js/runtime-main.ba2af1f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-deploy-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
